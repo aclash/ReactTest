0d4b94c9ae09f5d00885d24e262a417dc3c83f06
aclash <aclash@163.com>
2020-03-14 20:28:58 -0400

    1111

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0};
+  }
+  increment = (preStates) => {
+    this.setState((preStates) => { return {count: preStates.count + 1}});
+  }
+
+  decrement = (preStates) => {
+    this.setState((preStates) => { return {count: preStates.count - 1}});
+  }
+
+  reset = () => {
+    this.setState({count: 0});
+  }
+
+  render() {
+    return (
+      <div>
+        <button className='inc' onClick={this.increment}>Increment</button>
+        <button className='dec' onClick={this.decrement}>Decrement</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
